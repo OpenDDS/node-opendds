a0c624aca7808e5190d60798c72cfa14a3e66406
Timothy Simpson <simpsont@objectcomputing.com>
2021-12-03 09:55:04 -0600

    removing comments per review

--- test/run_test.pl
+++ test/run_test.pl
@@ -10,8 +10,6 @@ use PerlDDS::Run_Test;
 use strict;
 
 my $status = 0;
-#my $dcpsrepo_ior = "repo.ior";
-#unlink $dcpsrepo_ior;
 
 my $test = new PerlDDS::TestFramework();
 
@@ -38,18 +36,6 @@ if (exists($args{"node2cpp"})) {
 }
 
 $test->setup_discovery("-ORBDebugLevel 1 -ORBLogFile DCPSInfoRepo.log") unless $secure;
-
-#my $DCPSREPO;
-#if (not $secure) {
-#  $DCPSREPO = PerlDDS::create_process("$DDS_ROOT/bin/DCPSInfoRepo");
-#
-#  $DCPSREPO->Spawn();
-#  if (PerlACE::waitforfile_timed($dcpsrepo_ior, 30) == -1) {
-#      print STDERR "ERROR: waiting for Info Repo IOR file\n";
-#      $DCPSREPO->Kill();
-#      exit 1;
-#  }
-#}
 
 sub which {
   my $file = shift;
@@ -88,11 +74,6 @@ if ($sub_node) {
 }
 
 $test->process("sub", $sub_exec_name, $sub_args);
-#my $SUB = $test->create_process($sub_exec_name, $sub_args);
-#if ($sub_node) {
-#  $SUB->IgnoreExeSubDir(1);
-#}
-#$SUB->Spawn();
 
 my $pub_exec_name = "";
 my $pub_args = "";
@@ -110,31 +91,6 @@ if ($pub_node) {
 }
 
 $test->process("pub", $pub_exec_name, $pub_args);
-#my $PUB = $test->create_process($pub_exec_name, $pub_args);
-#if ($pub_node) {
-#  $PUB->IgnoreExeSubDir(1);
-#}
-#$my $PubResult = $PUB->SpawnWaitKill(60);
-#if ($PubResult != 0) {
-#    print STDERR "ERROR: " . $pub_exec_name . " returned $PubResult\n";
-#    $status = 1;
-#}
-
-#my $SubResult = $SUB->WaitKill(10);
-#if ($SubResult != 0) {
-#    print STDERR "ERROR: " . $sub_exec_name . " returned $SubResult\n";
-#    $status = 1;
-#}
-
-#if (not $secure) {
-#  my $ir = $DCPSREPO->TerminateWaitKill(5);
-#  if ($ir != 0) {
-#      print STDERR "ERROR: DCPSInfoRepo returned $ir\n";
-#      $status = 1;
-#  }
-#
-#  unlink $dcpsrepo_ior;
-#}
 
 $test->start_process("sub");
 $test->start_process("pub");
